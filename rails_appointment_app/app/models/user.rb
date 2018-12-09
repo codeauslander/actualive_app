d0be6749191826db7e8b25a7f8f15762bf79ebc8
codeauslander <cesar.gusto.catano@gmail.com>
2018-12-08 19:16:50 -0600

    Added Knock and Bcrypt gem

--- rails_appointment_app/app/models/user.rb
+++ rails_appointment_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many   :appointments
   has_many   :rooms, through: :appointments
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- rails_appointment_app/db/schema.rb
+++ rails_appointment_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181207015527) do
+ActiveRecord::Schema.define(version: 20181209010521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20181207015527) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.integer "kind"
     t.boolean "availability"
     t.datetime "created_at", null: false
